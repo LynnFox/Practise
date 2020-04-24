ac8b7ce2c0b39f2a88eb92324c2a744c885facc9
LynnFox <33144192+LynnFox@users.noreply.github.com>
2020-04-24 09:56:33 +0800

    Update 18.java

--- leetcode/offer/18.java
+++ leetcode/offer/18.java
@@ -1,3 +1,6 @@
+//考虑第一个节点有可能被删除，new一个额外的节点node1指向head作为新的链表头
+//然后，定义一个工作节点node2，迭代链表，删除目标节点
+//最后，返回node1.next
 class Solution {
     public ListNode deleteNode(ListNode head, int val) {
         ListNode node1 = new ListNode(-1);
