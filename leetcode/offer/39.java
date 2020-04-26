ee8a1590adc76c1b03e6cd64ea6d5606e36aadfb
LynnFox <33144192+LynnFox@users.noreply.github.com>
2020-04-26 17:10:35 +0800

    Update 39.java

--- leetcode/offer/39.java
+++ leetcode/offer/39.java
@@ -1,4 +1,4 @@
-//哈希计数
+//方案一：哈希计数
 //O(n)+O(n)
 class Solution {
     public static int majorityElement(int[] nums) {
@@ -20,12 +20,28 @@ class Solution {
     }
 }
 
-//排序
+//方案二：排序
 //O(nlogn)+O(logn)
 //emmmm,从来没有写过这么短的解题代码
 class Solution {
     public static int majorityElement(int[] nums) {
         Arrays.sort(nums);
         return nums[nums.length / 2];
+    }
+}
+//方案三：摩尔计数法
+//O(n)+O(1)
+class Solution {
+    public static int majorityElement(int[] nums) {
+        int res = nums[0];
+        int count = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                res = num;
+            }
+            count += (res == num) ? 1 : -1;
+        }
+        return res;
     }
 }
