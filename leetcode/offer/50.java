6356648f07c104e2c23dba5756f1cd8480249e8e
LynnFox <33144192+LynnFox@users.noreply.github.com>
2020-04-27 07:59:24 +0800

    Create 50.java

---
+++ leetcode/offer/50.java
@@ -0,0 +1,25 @@
+class Solution {
+    public char firstUniqChar(String s) {
+        if (s == null || s.length() == 0)
+            return ' ';
+
+        char[] str = s.toCharArray();
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char ch : str) {
+            if(!map.containsKey(ch)){
+                map.put(ch, 1);
+            } else{
+                map.put(ch, map.get(ch) + 1);
+            }
+        }
+
+        for(char ch : str){
+            if(map.get(ch) == 1){
+                return ch;
+            }
+        }
+
+        return ' ';
+    }
+}
