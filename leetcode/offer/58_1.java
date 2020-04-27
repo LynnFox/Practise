d34a04592c8a02cd60c168b9810587bcb8af35b9
LynnFox <33144192+LynnFox@users.noreply.github.com>
2020-04-27 11:46:49 +0800

    Update 58_1.java

--- leetcode/offer/58_1.java
+++ leetcode/offer/58_1.java
@@ -1,4 +1,5 @@
 //正则+split+StringBuilder
+//代码简单，但是耗时6ms
 class Solution {
     public String reverseWords(String s) {
         if (s == null || s.length() == 0)
@@ -14,4 +15,39 @@ class Solution {
         builder.deleteCharAt(builder.length() - 1);
         return builder.toString();
     }
+}
+
+//2ms
+class Solution {
+    public String reverseWords(String s) {
+        if (s.length() == 0) return "";
+
+        char[] st = s.toCharArray();
+        StringBuilder res = new StringBuilder();
+
+        for (int i = s.length() - 1, j = i; i >= 0 && j >= 0; ) {
+            //越过空格
+            while (j >= 0 && st[j] == ' ') {//循环后，j保存子串最后一个非空字符的位置
+                j--;
+            }
+
+            if (j < 0)
+                break;
+
+            i = j;
+
+            while (j >= 0 && st[j] != ' ') {//循环后，j保存子串第一个非空字符的下一个位置
+                j--;
+            }
+
+            //i:结束的位置
+            //j+1:开始的位置
+            //i-j:子串长度
+            res.append(st, j + 1, i - j).append(" ");
+            i = j;
+        }
+
+        //如果字符串只包含' ',返回""
+        return res.length() == 0 ? "" : res.substring(0, res.length() - 1);
+    }      
 }
