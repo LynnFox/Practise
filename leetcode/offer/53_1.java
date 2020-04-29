7e6519597ce97ccae157bb052ecb915b6e31ea8f
LynnFox <33144192+LynnFox@users.noreply.github.com>
2020-04-29 21:24:27 +0800

    Update 53_1.java

--- leetcode/offer/53_1.java
+++ leetcode/offer/53_1.java
@@ -13,3 +13,33 @@ class Solution {
 }
 
 //二分
+class Solution {
+    public int search(int[] nums, int target) {
+        //找到第一个目标的位置
+        int i = 0;
+        int j = nums.length - 1;
+        while (i <= j) {
+            int mid = (i + j) / 2;
+            if (nums[mid] >= target){
+                j = mid - 1;
+            } else{
+                i = mid + 1;
+            }
+        }
+        int left = i;
+
+        //找到最后一个目标的位置
+        j = nums.length - 1;
+        while(i <= j){
+            int mid = (i + j) / 2;
+            if(nums[mid] <= target){
+                i = mid + 1;
+            } else{
+                j = mid - 1;
+            }
+        }
+        int right = j;
+
+        return right - (left - 1);
+    }
+}
